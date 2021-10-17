de0f041f1a0ef357d31b86fcbdfd27d050adcd94
Philip Kung <pkung67@utexas.edu>
2021-10-16 22:44:57 -0500

    adding header switching code to get past ip blocker

--- api/app/scraper/crawlers/crawler.py
+++ api/app/scraper/crawlers/crawler.py
@@ -1,9 +1,18 @@
 import requests
+import random
 
 from bs4 import BeautifulSoup
 from abc import abstractmethod
 
 class Crawler:
+
+    USER_AGENT_LIST = [
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+    ]
 
     def __init__(self, base_url):
         self.base_url = base_url
@@ -16,6 +25,10 @@ class Crawler:
         return BeautifulSoup(html, 'html.parser')
 
     def get_html(self, url):
-        req = requests.get(url)
+        req = requests.get(url, headers={'User-Agent': self.random_agent()})
         if req.status_code == 200:
             return req.text
+
+    def random_agent(self):
+        idx = random.randint(0, len(self.USER_AGENT_LIST))
+        return self.USER_AGENT_LIST[idx]
